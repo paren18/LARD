f9da45e3ba3bdd08e521e554b10e56e04865e26f
paren18 <igor.zelenov.1998@mail.ru>
2024-02-28 22:59:51 +0300

    Испрвление V1

--- assets/captca.js
+++ assets/captca.js
@@ -1,6 +1,3 @@
 function onRecaptchaSuccess() {
-    document.getElementById("email").removeAttribute("disabled");
-    document.getElementById("password").removeAttribute("disabled");
-    document.getElementById("password2").removeAttribute("disabled");
     document.getElementById("submitBtn").removeAttribute("disabled");
 }
--- views/registration.tpl.php
+++ views/registration.tpl.php
@@ -8,18 +8,18 @@
         <form method="post" action="/registration">
             <div class="mb-3 pt-3">
                 <label for="email" class="form-label">Email</label>
-                <input name="email" type="email" class="form-control" id="email" aria-describedby="emailHelp" disabled>
+                <input name="email" type="email" class="form-control" id="email" aria-describedby="emailHelp" >
             </div>
             <div class="mb-3">
                 <label for="password" class="form-label">Пароль</label>
-                <input name="password" type="password" class="form-control" id="password" disabled>
+                <input name="password" type="password" class="form-control" id="password">
             </div>
             <div class="mb-3">
                 <label for="password_again" class="form-label">Повторите пароль</label>
-                <input name="password2" type="password" class="form-control" id="password2" disabled>
+                <input name="password2" type="password" class="form-control" id="password2">
             </div>
             <div class="g-recaptcha" data-sitekey="6LfycYIpAAAAALXpH8Ki6gTg07p4lCR3G5xzVBdz" data-callback="onRecaptchaSuccess"></div>
-            <button type="submit" class="btn btn-primary" id="submitBtn" disabled>Submit</button>
+            <button type="submit" class="btn btn-primary" id="submitBtn" disabled>Регестрация</button>
         </form>
         <?php if (!empty($message)): ?>
             <div class="text-danger bg-dark p-2 col-md-4 offset-md-4" style="text-align:center; font-size: 21px"><?= $message ?></div>
